4cd2242a539c26a502e668cbea2117a6de7286cd
Yaseen Lotfi <yalotfi@outlook.com>
2017-06-29 18:02:57 -0400

    test image processing, model loading, and model prediction

---
+++ test/model_test.py
@@ -0,0 +1,40 @@
+import numpy as np
+from keras.models import model_from_json
+from PIL import Image
+
+
+def load(json_path, weight_path):
+    json_file = open(json_path, 'r')
+    model_json = json_file.read()
+    json_file.close()
+    model = model_from_json(model_json)
+    model.load_weights(weight_path)
+    print("Loaded model architecture and weights from disk")
+    return model
+
+
+def process_img(img_path, res=(28, 28)):
+    # Read, convert to grayscale, and resize the image file
+    im = Image.open(img_path)
+    im = im.convert(mode='L')
+    im = im.resize(res, resample=Image.NEAREST)
+    print("Resolution: {0} \nColor Mode: {1}").format(im.size, im.mode)
+
+    # Reshape image into 4D tensor
+    tensor = np.array(im.getdata()).reshape(1, res[0], res[1], 1)
+    print(tensor.shape)
+    im.close()
+    return tensor
+
+
+def main():
+    tensor = process_img('mnist2.png')
+    model = load('../model/architecture.json', '../model/weights.h5')
+    probabilities = model.predict(tensor)
+    print(probabilities)
+    prediction = model.predict_classes(tensor)
+    print(prediction)
+
+
+if __name__ == '__main__':
+    main()
