a30b3541a2d96d43d5786d1a8ea40c47899da82b
Yaseen Lotfi <yalotfi@outlook.com>
2017-06-30 18:13:24 -0400

    Convert and save keras model to Core ML file for iOS app

--- model/keras_to_coreml.py
+++ model/keras_to_coreml.py
@@ -1,13 +1,24 @@
 import coremltools
-from keras.models import load_model
+from keras.models import model_from_json
 
 
-def convert_model(filepath):
+def convert_model(json_path, weight_path):
+    # Read model architecture configuration
+    json_file = open(json_path, 'r')
+    model_json = json_file.read()
+    json_file.close()
+
+    # Read trained weights and compile keras model
+    keras_model = model_from_json(model_json)
+    keras_model.load_weights(weight_path)
+    print("Loaded model architecture and weights from disk")
+
     # Convert Keras model to Core ML
-    keras_model = load_model(filepath)
     coreml_model = coremltools.converters.keras.convert(keras_model)
-    coreml_model.save('model.mlmodel')
+    print("Converted Keras model to Core ML model")
+    coreml_model.save('DigitClassifier.mlmodel')
+    print("Core ML model written to disk")
 
 
 if __name__ == "__main__":
-    convert_model("model.h5")
+    convert_model("architecture.json", "weights.h5")
