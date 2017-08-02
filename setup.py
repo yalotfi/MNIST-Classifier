94e0a99bd234513c7f6509bb335c122361654c53
Yaseen Lotfi <yalotfi@outlook.com>
2017-08-02 02:06:54 -0400

    setup configuration fleshed out

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 from setuptools import setup
+from setuptools import find_packages
 
 
 setup(
     name='mnist',
-    packages=[
-        'mnist',
-        'classifier'
-    ],
+    version='0.0.0',
+    description='Digit Classification App',
+    url='https://github.com/yalotfi/MNIST-Classifier',
+    author='Yaseen Lotfi',
+    license='MIT',
+    packages=find_packages(package_dir='mnist'),
     include_package_data=True,
     install_requires=[
-        'flask'
+        'flask',
+        'keras',
+        'tensorflow',
+        'PIL',
+        'numpy'
     ]
 )
