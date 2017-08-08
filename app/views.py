74a62144aef565d6b384427f56a87ae4559eb378
Yaseen Lotfi <yalotfi@outlook.com>
2017-08-07 22:40:53 -0400

    Flask app is working - problem with model prediction

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+import os
 import numpy as np
 import base64
+
 from PIL import Image
 from io import BytesIO
 
@@ -7,13 +9,8 @@ from flask import render_template
 from flask import request
 from flask import jsonify
 
-from Classifier.ModelIO import load_trained_model
+from app.Classifier.ModelIO import load_trained_model
 from app import app
-
-# Load model for inference
-architecture_path = 'model/architecture.json'
-weight_path = 'model/weights.h5'
-model = load_trained_model(architecture_path, weight_path)
 
 
 @app.route('/')
@@ -31,12 +28,22 @@ def upload():
     img = Image.open(BytesIO(base64.b64decode(img_base64)))
     img = img.convert(mode='L')
     img = img.resize((28, 28), resample=Image.NEAREST)
-    tensor = np.array(img.getdata()).reshape(1, 28, 28, 1)
+    tensor = np.invert(np.array(img.getdata()))
+    tensor = tensor.reshape(1, 28, 28, 1)
     img.close()
+
+    # Load model for inference
+    architecture_path = os.path.join('app', 'model', 'architecture.json')
+    weight_path = os.path.join('app', 'model', 'weights.h5')
+    model = load_trained_model(architecture_path, weight_path)
 
     # Inference time: POST probability vector and predicted class
     probs = model.predict(tensor)
     preds = model.predict_classes(tensor)
+    print(probs)
+    print(preds)
+
+    # Send JSON to the client
     return jsonify(status='got img',
-                   probabilities=probs,
-                   predicted_class=preds)
+                   probabilities=probs.tolist()[0],
+                   predicted_class=preds.tolist()[0])
